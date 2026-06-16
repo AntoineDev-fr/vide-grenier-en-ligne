4384041c0a5561e0e077546f8d8d83f749023620
antoine <antoinelimare76700@gmail.com>
2026-06-16 09:23:46 +0200

    3 co,nteneur docker

--- App/Config.php
+++ App/Config.php
@@ -2,15 +2,15 @@
 
 namespace App;
 
+/**
+ * Application configuration
+ *
+ * PHP version 7.0
+ */
 class Config
 {
-    const DB_HOST = 'db-dev';
-    const DB_NAME = 'videgrenier';
-    const DB_USER = 'user';
-    const DB_PASSWORD = 'password';
-    
-  
-  fix/remember-me
+    const APP_ENV = 'dev';
+
     /**
      * Database host
      * @var string
@@ -38,6 +38,50 @@ class Config
     /**
      * Show or hide error messages on screen
      * @var boolean
-     */ develop
+     */
     const SHOW_ERRORS = true;
+
+    public static function getAppEnv()
+    {
+        return self::getEnv('APP_ENV', self::APP_ENV);
+    }
+
+    public static function getDbHost()
+    {
+        return self::getEnv('DB_HOST', self::DB_HOST);
+    }
+
+    public static function getDbName()
+    {
+        return self::getEnv('DB_NAME', self::DB_NAME);
+    }
+
+    public static function getDbUser()
+    {
+        return self::getEnv('DB_USER', self::DB_USER);
+    }
+
+    public static function getDbPassword()
+    {
+        return self::getEnv('DB_PASSWORD', self::DB_PASSWORD);
+    }
+
+    public static function showErrors()
+    {
+        return filter_var(
+            self::getEnv('APP_SHOW_ERRORS', self::SHOW_ERRORS ? 'true' : 'false'),
+            FILTER_VALIDATE_BOOLEAN
+        );
+    }
+
+    private static function getEnv($key, $default)
+    {
+        $value = getenv($key);
+
+        if ($value === false || $value === '') {
+            return $default;
+        }
+
+        return $value;
+    }
 }
--- App/Controllers/Product.php
+++ App/Controllers/Product.php
@@ -11,6 +11,7 @@ use \Core\View;
  */
 class Product extends \Core\Controller
 {
+    const CONTACT_SUCCESS_MESSAGE = 'Votre message a bien ete valide.';
 
     /**
      * Affiche la page d'ajout
@@ -49,18 +50,79 @@ class Product extends \Core\Controller
     public function showAction()
     {
         $id = $this->route_params['id'];
+        $contactForm = $this->getDefaultContactFormData();
+        $contactErrors = [];
+        $contactSuccess = null;
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['contact_submit'])) {
+            $contactForm = $this->getContactFormData($_POST);
+            $contactErrors = $this->validateContactForm($contactForm);
+
+            if (empty($contactErrors)) {
+                $contactSuccess = self::CONTACT_SUCCESS_MESSAGE;
+                $contactForm = $this->getDefaultContactFormData();
+            }
+        }
 
         try {
-            Articles::addOneView($id);
             $suggestions = Articles::getSuggest();
             $article = Articles::getOne($id);
+
+            if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+                Articles::addOneView($id);
+                if (!empty($article)) {
+                    $article[0]['views']++;
+                }
+            }
         } catch(\Exception $e){
             var_dump($e);
         }
 
         View::renderTemplate('Product/Show.html', [
             'article' => $article[0],
-            'suggestions' => $suggestions
+            'suggestions' => $suggestions,
+            'contactForm' => $contactForm,
+            'contactErrors' => $contactErrors,
+            'contactSuccess' => $contactSuccess
         ]);
+    }
+
+    private function getDefaultContactFormData()
+    {
+        return [
+            'name' => isset($_SESSION['user']['username']) ? $_SESSION['user']['username'] : '',
+            'email' => '',
+            'message' => ''
+        ];
+    }
+
+    private function getContactFormData($data)
+    {
+        return [
+            'name' => trim($data['contact_name'] ?? ''),
+            'email' => trim($data['contact_email'] ?? ''),
+            'message' => trim($data['contact_message'] ?? '')
+        ];
+    }
+
+    private function validateContactForm($data)
+    {
+        $errors = [];
+
+        if ($data['name'] === '') {
+            $errors[] = 'Le nom est obligatoire.';
+        }
+
+        if ($data['email'] === '') {
+            $errors[] = "L'email est obligatoire.";
+        } elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
+            $errors[] = "L'email est invalide.";
+        }
+
+        if ($data['message'] === '') {
+            $errors[] = 'Le message est obligatoire.';
+        }
+
+        return $errors;
     }
 }
